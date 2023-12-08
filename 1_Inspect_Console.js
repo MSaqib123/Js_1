81c5647ad5899996be38d2530e182acc55f1f86e
MBoota <m43577535@gmail.com>
2023-12-08 19:29:05 +0500

    starting js

---
+++ 1_Inspect_Console.js
@@ -0,0 +1,11 @@
+//_________ Working in  Inspect tole in browser ______________
+alert("Pakisnta")
+let js = "Awesome"
+
+if(js==="Awesome")
+    alert("Pakistan zindbade")
+
+
+const sql = "Boring"
+if(sql == "Boring")
+    alert("I love u my brother")
