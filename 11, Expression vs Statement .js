df9a74f7f556b89459577e6afcfa3850d747f923
MBoota <m43577535@gmail.com>
2023-12-09 16:40:22 +0500

    Expression vs Satement

---
+++ 11, Expression vs Statement .js
@@ -0,0 +1,11 @@
+//___________ Expression  (Produc Value) _____________
+53+52
+true && false && !false
+
+
+//___________ Statement  (Perform Action) _____________
+if(25 == 25)
+{
+    var n = "pakistan"
+}
+
