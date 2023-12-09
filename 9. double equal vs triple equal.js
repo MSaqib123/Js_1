8937e9047eee2598e547bd6208c8374bb0bea045
MBoota <m43577535@gmail.com>
2023-12-09 15:59:27 +0500

    double equal and triple equal

---
+++ 9. double equal vs triple equal.js
@@ -0,0 +1,23 @@
+//___________________ doulbe equal === ________________________
+//double equal does not check   datatype
+let age = 25;
+
+if(age == '25'){
+
+    console.log("match")
+}
+
+
+
+//___________________ doulbe equal === ________________________
+//double equal does not check   datatype
+
+if(age === '25')
+{
+
+    console.log("match")
+}
+else {
+
+    console.log("\nNotMatch")
+}
