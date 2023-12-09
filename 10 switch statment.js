7ed82ef89ff28752b97c9209bfc4317e1eafc0c1
MBoota <m43577535@gmail.com>
2023-12-09 16:09:39 +0500

    switch statement

---
+++ 10 switch statment.js
@@ -0,0 +1,19 @@
+let day = "Monday";
+
+switch(day){
+    case day == "Monday":
+    case day == "Tuesday":
+    case day == "Wednesday":
+    case day == "Thirsday":
+    case day == "Friday":
+        console.log("Working day")
+        break;
+
+    case day == "Saterday":
+        console.log("Working Day in Home")
+        break;
+
+    default:
+        console.log("HolyDay")
+}
+    
