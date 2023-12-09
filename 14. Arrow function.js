1d72d138fa2ac30eb04aef98473dc7961aa44de0
MBoota <m43577535@gmail.com>
2023-12-09 20:03:59 +0500

    arrow function

---
+++ 14. Arrow function.js
@@ -0,0 +1,9 @@
+//_________ Arrow function very importent ___________
+
+//__________ Simple fn _________
+//decl
+const calcAge= birthYeah => 2023-birthYeah; //very simple
+
+//use
+console.log(calcAge(1998))
+
