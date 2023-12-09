f4120888ed534b15f85af5de507d850b0f457733
MBoota <m43577535@gmail.com>
2023-12-09 11:35:07 +0500

    class 3

---
+++ 3. primitive datatype.js
@@ -0,0 +1,17 @@
+//------------- Data Type  Accept  Object  --------------
+//int 
+//string
+//bool
+//null
+//undifned
+//symbol
+//bingint
+//dynamic data typing 
+
+console.log(typeof "Pakistan")
+console.log(typeof 23432)
+console.log(typeof true)
+console.log(typeof 'jons')
+
+let year;
+console.log(typeof year)
