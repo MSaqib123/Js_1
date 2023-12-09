402c1b78f953c7d19a9a0d5da02d758c1b5cd637
MBoota <m43577535@gmail.com>
2023-12-09 15:53:06 +0500

    type conversion

--- 8. type conversion.js
+++ 8. type conversion.js
@@ -1,5 +1,21 @@
+//____________ integer _______________
 let v1 = '25'
 console.log('v1 ' , typeof v1 , (v1 + 1))
 
 v1 = Number(v1)
 console.log('v1', typeof v1 , (v1 + 1))
+
+v2 = prompt("Enter Number")
+v2 = parseInt(v2)
+v3 = v1 + v2;
+console.log("sum of v1 , v2 : " , v3)
+
+v4 = prompt("Enter Number")
+v4 = parseFloat(v2)
+v5 = v1 + v4;
+console.log("sum of v1 , v2 : " , v3)
+
+//____________ string _______________
+name = prompt("Enter Name")
+name = String(name)
+console.log("sum name : " , v3)
