e955dc8115aac54b8790ef308a508a5b7e87e99d
MBoota <m43577535@gmail.com>
2023-12-09 21:20:56 +0500

    multiline arrow fn

--- 14. Arrow function.js
+++ 14. Arrow function.js
@@ -1,9 +1,18 @@
 //_________ Arrow function very importent ___________
 
-//__________ Simple fn _________
+//__________ Arrow FN _________
 //decl
 const calcAge= birthYeah => 2023-birthYeah; //very simple
 
 //use
 console.log(calcAge(1998))
 
+
+//__________ Multiline Arrow Fn ___________
+const calcReturedYear= birthYeah => {
+    let age = (2023 - birthYeah);
+    let retuiredYear = 40-age;
+    return retuiredYear;
+}
+
+console.log("Retured Year remaing : " , calcReturedYear(1996))
