a62d6d4240a267ddfe6a700482e8badd7a9b5e60
MBoota <m43577535@gmail.com>
2023-12-09 14:33:00 +0500

    string vs template string

---
+++ 6. String vs string Template.js
@@ -0,0 +1,22 @@
+//____________ 1. String _____________________
+let paragraph = "I am a big fan of shid khan afridy  but i am upset he is injerd and can not participate in  PSL"
+        +"because he has returied from domastic cricket as wel as"
+console.log(paragraph);
+
+paragraph = "I am a big fan of shid khan afridy  but i am upset he is injerd and can not participate in  PSL \n\
+        because he has returied from domastic cricket as wel as"
+console.log(paragraph);
+
+//____________ 2. String Templete _____________________
+paragraph = `I am a big fan of shid khan afridy  but i am upset he is injerd and can not participate in  PSL
+            because he has returied from domastic cricket as wel as , sahid afriday is very good cricketer 
+            and all fans love to shahid khaan afridy`
+console.log(paragraph);
+
+
+//____________ 3. String Templete + Variable _____________________
+var name = "Shahid Khaan Afridi"
+paragraph = `I am a big fan of ${name}  but i am upset he is injerd and can not participate in  PSL
+            because he has returied from domastic cricket as wel as , ${name} is very good cricketer 
+            and all fans love to ${name} , but as i consert ${name} has good personality as well as.`
+console.log(paragraph);
