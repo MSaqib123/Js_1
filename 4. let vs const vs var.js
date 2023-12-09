0fa2f503e5de94b9c516392e8d472a06053f04b3
MBoota <m43577535@gmail.com>
2023-12-09 11:45:14 +0500

    class 4

---
+++ 4. let vs const vs var.js
@@ -0,0 +1,21 @@
+//___________ES-6  feature _________________
+//___________ let , const _____________
+let latVariable = 234;
+console.log(6)
+
+
+const fiexedValueVar = 44;
+console.log(fiexedValueVar);
+
+//fiexedValueVar = 4;   error  const  variable can not  reassign
+
+
+//___________ES-5 _________________ 
+var anyDatatypeVariable = 'Pakista'
+console.log(anyDatatypeVariable)
+
+var anyDatatypeVariable = true
+console.log(anyDatatypeVariable)
+
+var anyDatatypeVariable = 234
+console.log(anyDatatypeVariable)
