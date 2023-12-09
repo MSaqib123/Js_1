cfa03f6d86a56666897f65f68a1994efa43ce173
MBoota <m43577535@gmail.com>
2023-12-10 01:48:52 +0500

    Array

---
+++ 18. Array.js
@@ -0,0 +1,31 @@
+const friend1 = "Saqib"
+const friend2 = "Noman"
+const friend3 = "Ali"
+
+//______________ Array 1 __________________
+//to store multiple values in  single  variable
+const friends = ["Shazad","NOman","Huzafaa"]
+//print array
+console.log(friends)
+//print single Values
+console.log(friends[0])
+console.log(friends[1])
+console.log(friends[2])
+
+//______________ Array 2 __________________
+const years = new Array(1991,1984,2008,2020);
+console.log(years[0])
+console.log(years[1])
+console.log(years[2])
+
+//______________ Array length __________________
+console.log(years.length)
+
+//______________ Array last element position __________________
+console.log(years.length-1)
+
+//______________ Array last element __________________
+console.log(years[length-1])
+
+//______________ Replace Array Element  __________________
+friends[1] = "Talha"
