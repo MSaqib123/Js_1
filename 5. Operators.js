46c0e4bc5d16bc1dea3f4b9797437e262c8bc596
MBoota <m43577535@gmail.com>
2023-12-09 12:10:53 +0500

    Operators

---
+++ 5. Operators.js
@@ -0,0 +1,41 @@
+//___________ 1. Arthimatics ____________________
+let add = 1+1
+let sub = 5-2
+let mult = 5*2
+let divid = 5/2
+let modulas = 5%2
+let power = 5**2;
+
+//____________ 2. concatination __________
+let fname = "M"
+let lName = "Saqib"
+console.log(fname + " " + lName)
+
+//____________ 3 Assignemnt Op __________
+let x = 10 + 5;
+x += 25; // x = x + 25
+x *= 25; // x = x * 25
+x++;  // x+1  postfixed
+x--;  // x-1  postfixed
+++x;  // 1+x  prefixed
+--x;// 1-x  prefixed
+
+//____________ 4 Compairsingo  Op __________
+let a = 55;
+let b = 55;
+let result = (a==b);
+result = (a!=b)
+result = (a>b)
+result = (a<b)
+result = (a>=b)
+result = (a<=b)
+
+//____________ 5 Logical Op __________
+let m = 55;
+let n = 55;
+result = (m==55 && n != 55)
+result = (m==55 || n != 55)
+
+
+
+
