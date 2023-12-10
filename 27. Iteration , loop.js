5d22bb825990a9ebce51dae5f2d71ea91ba37644
MBoota <m43577535@gmail.com>
2023-12-10 15:01:59 +0500

    looop , iteration

---
+++ 27. Iteration , loop.js
@@ -0,0 +1,10 @@
+
+//++++++++++++++++++++ Controll structure ==========================
+//1.  if  , swatch  ..  controll structure
+//2. loop is also  controll structre
+
+//________________ iteration _____________________
+for(let rep=0 ; rep < 10 ; rep++){
+    count = rep;
+    console.log("Lifting weights repetition " + (++count))
+}
