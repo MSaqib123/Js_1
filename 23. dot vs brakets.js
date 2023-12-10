8b81130ecf5ce95b2aebfa86bc813a8f1798344f
MBoota <m43577535@gmail.com>
2023-12-10 12:33:53 +0500

    dot vs brackete notation object

---
+++ 23. dot vs brakets.js
@@ -0,0 +1,23 @@
+
+const developer = {
+    firstName:'Saqib',
+    lastName : 'Ali',
+    age : 2023 - 1996,
+    jobs : 'developer',
+    friends:['Michael','Peter','Steven']
+}
+
+//_________________ show as object __________________
+console.log(developer)
+
+//_________________ destructure the object __________________
+console.log(developer.firstName)
+console.log(developer.lastName)
+console.log(developer.age)
+console.log(developer.friends[0],developer.friends[1],developer.friends[2])
+
+
+
+// var objFname = developer.fName;
+// var objLname = developer.lastName;
+// console.log(objFname,objLname)
