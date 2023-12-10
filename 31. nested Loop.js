0daaa741999cd5e2d05e4a04434953e3b7d08197
MBoota <m43577535@gmail.com>
2023-12-10 16:54:43 +0500

    Nested Loop

---
+++ 31. nested Loop.js
@@ -0,0 +1,8 @@
+for (let exer = 0; exer < 3; exer++) {
+    let ex = exer;
+    console.log("--------- Exercice start : " + (++ex) )
+    for (let rep = 0; rep < 6; rep++) {
+        let r = rep;
+        console.log("Lifting Weight : " + (++r) )
+    }
+}
