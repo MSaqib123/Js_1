0bb3f7d50f33a8f4721c67dd98711f61d371f279
MBoota <m43577535@gmail.com>
2023-12-10 15:20:10 +0500

    Array and Loop

---
+++ 28. Array and Loop.js
@@ -0,0 +1,13 @@
+const st = [
+    'Saqib',
+    'Afzal',
+    2037-1996,
+    'developer',
+    ["Talhaa bhi","Chota sain","Huzaifa"],
+    true
+]
+
+for(let i = 0 ; i< st.length ; i++){
+    console.log(st[i] , typeof st[i])
+}
+
