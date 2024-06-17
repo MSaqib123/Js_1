e2170232d44a5682e396f125c161ba0da07e371b
M Boota <48630668+MSaqib123@users.noreply.github.com>
2024-06-17 19:06:47 +0500

    Intro

---
+++ 0. introduction.js
@@ -0,0 +1,23 @@
+//__________ what is  Javascript ______________
+/*
+    Javascrip is  
+     Heigh Level , Object oriented , Multi-paradium 
+    programing language
+*/
+
+
+//___ Programing Lnaauge ___
+/*
+    a tool  
+    that allow us to write code 
+    or wo code Computer ko  samjhyaa  or krna ka lyaa khaaa ka manaa kya Task perform krna ha
+    ...
+    to Comunicate with computer
+ */
+
+//___ Heigh Level Lanaguege ___
+//js is Heigh level  interprated lanague
+//means is me  complexity ny haa  for Memeory Managmeent like  stack and heep
+
+//____ Object oriented ES6_____ 
+//js me Hm  Object base bhi  work krta han
