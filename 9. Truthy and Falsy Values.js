e4d04cc5c20b40ee39ebe1fbb65207f68657634a
M Boota <48630668+MSaqib123@users.noreply.github.com>
2024-06-17 20:37:06 +0500

    truthy and falsy values

--- 9. Truthy and Falsy Values.js
+++ 9. Truthy and Falsy Values.js
@@ -1,23 +1,41 @@
 //_________________ Falsy Values ___________
 //in Js ther are  5 value  are falsy
 
-//______ 1. _______
+//______ 1. (0) _______
 // 0
 console.log(Boolean(0))
 
-//______ 2. _______
+let money = 100; //true     //0; //false
+if(money){
+    console.log("please don't speend all mony")
+}
+else{
+    console.log("YOu should get job")
+}
+
+
+
+//______ 2. ('') _______
 // ''
 console.log(Boolean(''))
 
-//______ 3. _______
+//______ 3. (undefined) _______
 //undefined
 console.log(Boolean(undefined))
 
-//______ 4. _______
+let height;
+if(height){
+    console.log("value is defined")
+}
+else{
+    console.log("value is Undefined")
+}
+
+//______ 4. (null) _______
 //null
 console.log(Boolean(null))
 
-//______ 5. _______
+//______ 5. (NaN) _______
 //NaN   not a number
 console.log(Boolean(NaN))
 
