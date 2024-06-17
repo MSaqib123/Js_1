5437fa380ce97027091eb8d17f33a6b393811b92
M Boota <48630668+MSaqib123@users.noreply.github.com>
2024-06-17 19:16:08 +0500

    big changes

---
+++ 14. Function.js
@@ -0,0 +1,27 @@
+fullName("M","Saqib")
+fullName("M","skdfds")
+fullName("M","sdfsd")
+fullName("KOkab","Alil")
+
+//___________ 1. take some thing , Return nothing ____________
+function fullName(fName,lName){
+    console.log(fName + " " + lName)
+}
+
+//___________ 2. take some thing , Return somthing ____________
+function returnfullName(fName,lName){
+    return (fName + " " + lName)
+}
+console.log(returnfullName("PK ","Pakistan"))
+
+//___________ 3. take nothing, Return somthing ____________
+function ff(){
+    return ("function is awain")
+}
+console.log(ff())
+
+//___________ 4. take nothing , return nothing ____________
+function myAlert(){
+    alert("Pakistna zindab")
+}
+myAlert()
---
+++ 9. Truthy and Falsy Values.js
@@ -0,0 +1,26 @@
+//_________________ Falsy Values ___________
+//in Js ther are  5 value  are falsy
+
+//______ 1. _______
+// 0
+console.log(Boolean(0))
+
+//______ 2. _______
+// ''
+console.log(Boolean(''))
+
+//______ 3. _______
+//undefined
+console.log(Boolean(undefined))
+
+//______ 4. _______
+//null
+console.log(Boolean(null))
+
+//______ 5. _______
+//NaN   not a number
+console.log(Boolean(NaN))
+
+
+//_________________ truthy Values ___________
+// true , value , 'string'   , {} 
