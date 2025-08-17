1930c80c0a37373b38a2c20c78275a537e14cffc
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-08-17 09:53:23 +0500

    Expression and Statement confusion

--- 12, Expression vs Statement .js
+++ 12, Expression vs Statement .js
@@ -1,14 +1,22 @@
 //___________ Expression  (return any Value) _____________
+// 🧮 Expression (3 Points)
+// Expression hamesha koi value return karti hai (jaise 5 + 3 → 8).
+// Ye calculation ya comparison karne ke liye hoti hai.
+// Expression ko hum variable me store bhi kar sakte hain (jaise let x = 5 + 2;).
 53+52
 true && false && !false
 
 
 //___________ Statement  (Perform Action) _____________
+// ⚙️ Statement (3 Points)
+// Statement sirf action perform karti hai, value return nahi karti.
+// Ye program ka flow control karti hai (jaise if, for, while).
+// Statement ke andar expressions likhe ja sakte hain, lekin wo khud expression nahi hoti.
+
 if(25 == 25)
 {
     var n = "pakistan"
 }
 
-//_______ understand the confuseion ______
-//======= Pakistani Jugar me =========
+
 
