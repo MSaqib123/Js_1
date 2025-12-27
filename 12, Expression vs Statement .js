3b542db3cab0aff1ec8fca355ed0b79b9f4b8217
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-12-27 17:22:15 +0500

    Enhance explanations for expressions and statements in the code comments

--- 12, Expression vs Statement .js
+++ 12, Expression vs Statement .js
@@ -1,4 +1,7 @@
 //___________ Expression  (return any Value) _____________
+// Expression  = produce value
+// easy words me expression wo ha jo kuch value deta ha
+
 // 🧮 Expression (3 Points)
 // Expression hamesha koi value return karti hai (jaise 5 + 3 → 8).
 // Ye calculation ya comparison karne ke liye hoti hai.
@@ -8,15 +11,18 @@ true && false && !false
 
 
 //___________ Statement  (Perform Action) _____________
+// Statement   = perform action
+// // statement wo ha jo kuch krta ha        
+
 // ⚙️ Statement (3 Points)
 // Statement sirf action perform karti hai, value return nahi karti.
 // Ye program ka flow control karti hai (jaise if, for, while).
 // Statement ke andar expressions likhe ja sakte hain, lekin wo khud expression nahi hoti.
 
-if(25 == 25)
-{
-    var n = "pakistan"
-}
+    let exp = 3 + 4;  // ye ek expresison ha kyun k ye value produce kr rha ha
 
 
+    if (exp > 5) {    // ye ek statement ha kyun k ye ek amal kr rha ha
+        console.log('exp is greater than 5');
+    }
 
